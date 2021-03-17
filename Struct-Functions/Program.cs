87527de769bbe1cd88fe4e2db32399b98ac3a74c
Daniel <daniel.simonovski24@gmail.com>
2021-03-17 15:56:37 +0100

    start

---
+++ Struct-Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Functions
+{
+    class Program
+    {
+        struct CustomerName
+        {
+            public string firstName, lastName;
+            public string Name() => firstName + " " + lastName;
+        }
+
+        static void Main(string[] args)
+        {
+            CustomerName myCustomer;
+            myCustomer.firstName = "Daniel";
+            myCustomer.lastName = "Simonovski";
+            Console.WriteLine($"{myCustomer.firstName} {myCustomer.lastName}");
+            Console.WriteLine(myCustomer.Name());
+            Console.ReadLine();
+        }
+    }
+}
